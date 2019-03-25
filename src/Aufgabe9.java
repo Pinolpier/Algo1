8d71b9705f60c3d0b2bc5ed927cdb9ad3f8be9fc
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2019-03-25 16:11:27 +0100

    Fixed Aufgabe9 so every mathematically allowed matrix multiplication can be performed correctly - tested with two different matrices but no JUnit so far

--- src/Aufgabe9.java
+++ src/Aufgabe9.java
@@ -2,40 +2,26 @@ import java.util.Arrays;
 
 public class Aufgabe9 {
     public static void main(String[] args) {
-        double[][] a = {{1,2,3}, {3,4,5}};
-        double[][] b = {{5,6}, {7,8}, {9,10}};
-        /*double[][] product = {{0,0}, {0,0}};
-
-        for (int i = 0; i < a.length; i++) {
-            double sum = 0;
-            for (int j = 0; j < a[i].length; j++) {
-                for (int k = 0; k < a[i].length; k++) {
-                    sum += a[i][k]*b[k][j];
-                }
-                product[i][j] = sum;
-                sum = 0;
-            }
-        }
-        System.out.println(Arrays.deepToString(product));
-        System.out.println("Alte Mathode done!");*/
-        System.out.println("Neue Methode: " + Arrays.deepToString(matrixMultiplication(a, b)));
+        double[][] a = {{1,2}, {3,4}};
+        double[][] b = {{5,6}, {7,8}};
+        System.out.println(Arrays.deepToString(matrixMultiplication(a, b)));
     }
 
-    public static double[][] matrixMultiplication(double[][] matrixA, double[][] matrixB) throws IllegalArgumentException {
+    private static double[][] matrixMultiplication(double[][] matrixA, double[][] matrixB) throws IllegalArgumentException {
         //In dieser Schleife wird überprüft, ob die übergebenen Arrays gültige Matrizen darstellen
-        for (double[] b : matrixB) {
-            if (b.length != matrixA.length) {
-                throw new IllegalArgumentException("Die Anzahl der Zeilen von Matrix a ( " + matrixA.length + " ) entspricht nicht der Anzahl der Zeilen von Matrix b ( " + b.length + " )! Eine Matrixmultiplikation ist in diesem Fall nicht definiert!");
+        for (double[] a : matrixA) {
+            if (a.length != matrixB.length) {
+                throw new IllegalArgumentException("Die Anzahl der Zeilen von Matrix a ( " + matrixB.length + " ) entspricht nicht der Anzahl der Zeilen von Matrix b ( " + a.length + " )! Eine Matrixmultiplikation ist in diesem Fall nicht definiert!");
             }
         }
         //Wenn die Matrizen gültig sind wird ein Ausgabe Array der passenden Dimension erstellt.
-        double[][] ergebnis = new double[matrixB[0].length][matrixA.length];
-        for (int i = 0; i < ergebnis[0].length; i++) { //Zeilen der Ergebnismatrix nacheinander durchiterieren
-            for (int j = 0; j < ergebnis.length; j++) { //Spalten der Ergebnismatrix nacheinander durchiterieren
+        double[][] ergebnis = new double[matrixA.length][matrixB[0].length];
+        for (int i = 0; i < ergebnis.length; i++) { //Spalten der Ergebnismatrix nacheinander durchiterieren
+            for (int j = 0; j < ergebnis[0].length; j++) { //Zeilen der Ergebnismatrix nacheinander durchiterieren
                 //Im folgenden der Code um das Ergebnis je Feld zu berechnen
                 double sum = 0.0;
-                for (int m = 0; m < matrixA.length; m++) {
-                    sum+=matrixA[m][i]*matrixB[j][m];
+                for (int m = 0; m < matrixA[0].length; m++) {
+                    sum+=matrixA[i][m]*matrixB[m][j];
                 }
                 ergebnis[i][j] = sum;
             }
@@ -43,5 +29,3 @@ public class Aufgabe9 {
         return ergebnis;
     }
 }
-
-// [[58.0, 76.0], [76.0, 100.0]]
