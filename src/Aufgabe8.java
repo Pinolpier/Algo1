19a9bc4598367dc4f1218d3b0976e383b57c4720
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2019-03-25 16:23:32 +0100

    Finished Aufgabe8 and checked it's working for the given example - in case of this task this is enough testing so it works always

--- src/Aufgabe8.java
+++ src/Aufgabe8.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+
 public class Aufgabe8 {
     public static void main(String[] args) {
-        int[][] a = {{1,2}, {3,4}, {5,6}};
-        int b = 2;
+        double[][] a = {{1,2}, {3,4}, {5,6}};
+        double b = 2;
+
+        double[][] solution = scalarMultiplication(a, b);
+        System.out.println(Arrays.deepToString(a));
+        System.out.println(Arrays.deepToString(solution));
+    }
+
+    private static double[][] scalarMultiplication(double[][] a, double b) {
+        double[][] solution = new double[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                solution[i][j] = a[i][j] * b;
+            }
+        }
+        return solution;
     }
 }
