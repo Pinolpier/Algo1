9be7d287fa4523d9c04df72563271c84ee906f5c
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2019-03-26 17:26:30 +0100

    Final fix for task during class - now working

--- src/Aufgabe1_20190326.java
+++ src/Aufgabe1_20190326.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class Aufgabe1_20190326 {
     public static void main(String[] args) {
-        System.out.println(toDecimal("z", 46));
+        System.out.println(toBase(toDecimal("1256f", 16), 16));
+        System.out.println(toBase(toDecimal("11011011", 10), 10));
     }
     private static int toDecimal(String number, int base) {
         char letter;
@@ -16,11 +19,11 @@ public class Aufgabe1_20190326 {
                 }
             } else {
                 if (letter >= 48 && letter <= 57) {
-                    result += (letter-48) * (int) Math.pow(base, i);
+                    result += (letter-48) * (int) Math.pow(base, number.length() - 1 - i);
                 } else {
                     letter = Character.toLowerCase(letter);
                     if (letter >= 97 && letter <= 97 + base - 10) {
-                        result += (letter-87) * (int) Math.pow(base, i);
+                        result += (letter-87) * (int) Math.pow(base, number.length() - 1 - i);
                     } else {
                         System.out.println("Nicht erlaubter Character: " + letter + " ist ignoriert und als 0 betrachtet worden!");
                     }
@@ -28,5 +31,29 @@ public class Aufgabe1_20190326 {
             }
         }
         return result;
+    }
+
+    private static String toBase(int decimal, int base) {
+        ArrayList<Integer> reste = new ArrayList<>();
+        int rest;
+        while (decimal != 0) {
+            rest = decimal % base;
+            reste.add(rest);
+            decimal = (decimal) / base;
+        }
+        String result = "";
+        for (Integer i : reste) {
+            if (i <= 9) {
+                result += i;
+            } else {
+                result += (char) (97 + i - 10);
+            }
+        }
+        //System.out.println(result);
+        String reversed = "";
+        for (int i = result.length() - 1; i >= 0; i--) {
+            reversed += result.charAt(i);
+        }
+        return reversed;
     }
 }
